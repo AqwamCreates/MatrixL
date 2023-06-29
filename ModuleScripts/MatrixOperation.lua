c598325277ec8878b1492c77d7a41ae90f8749a0
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-06-29 14:57:44 +0800

    Update MatrixOperation.lua

--- ModuleScripts/MatrixOperation.lua
+++ ModuleScripts/MatrixOperation.lua
@@ -18,14 +18,16 @@ local matrixOperationFunction = {
 	
 	['exp'] = function (x, y) return (math.exp(x, y)) end,
 	['e'] = function (x, y) return (math.exp(x, y)) end,
+	['exponent'] = function (x, y) return (math.exp(x, y)) end,
 	
-	['^'] = function (x, y) return (x ^ y) end,
-	['power'] = function (x, y) return (x ^ y) end,
-	['exponent'] = function (x, y) return (x ^ y) end,
+	['^'] = function (x, y) return (math.pow(x, y)) end,
+	['power'] = function (x, y) return (math.pow(x, y)) end,
 	
 }
 
 function module:matrixOperation(operation, matrix1, matrix2)
+	
+	if (#matrix1 ~= #matrix2) or (#matrix1[1] ~= #matrix2[1]) then error("Incompatible Dimensions! (" .. #matrix1 .." x " .. #matrix1[1] .. ") and (" .. #matrix2 .. " x " .. #matrix2[1] .. ")") end
 	
 	local maxRow = math.max(#matrix1, #matrix2)
 	local maxColumn = math.max(#matrix1[1], #matrix2[1])
